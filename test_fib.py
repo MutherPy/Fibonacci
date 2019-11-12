a620236dab87558efb9acd8e09ca2683921477c0
MutherPy <MutherPy@users.noreply.github.com>
2019-11-12 10:27:36 +0200

    Update test_fib.py

--- test_fib.py
+++ test_fib.py
@@ -1,13 +1,13 @@
 import unittest
 import fibonacci as fib
 
-class FibonTest(unittest.TestCase):
-    """ Fibonacci numbers test """
+class FibonErrorsTest(unittest.TestCase):
+    """ Fibonacci for errors test """
         
     @classmethod
     def setUpClass(cls):
         """ Set up for class """
-        print('\n Fibonacci test start: ')
+        print('\n Fibonacci test for errors start: ')
         print(' +++++++++++++ ')
         
     @classmethod
@@ -19,7 +19,6 @@ class FibonTest(unittest.TestCase):
     def setUp(self):
         """ For comfort """
         print(' --> ')
-
 
     def test_argument_string(self):
         """ Current arg is string. TypeError """
@@ -41,14 +40,68 @@ class FibonTest(unittest.TestCase):
         """ Current arg is negative. RecursionError """
         self.assertRaises(RecursionError, fib.fibon, -3)
 
-    def test_return_amount(self):
-        """ Correct return length """
-        self.assertEqual(len(fib.fibon(10)), 10)
+    def test_argument_float(self):
+        """ Current arg is float. RecursionError """
+        self.assertRaises(RecursionError, fib.fibon, 2.5)
+
+    def test_argument_negativefloat(self):
+        """ Current arg is float. RecursionError """
+        self.assertRaises(RecursionError, fib.fibon, -2.5)
+        
+
+class FibonNoneTest(unittest.TestCase):
+    """ Fibonacci for none test """
+    @classmethod
+    def setUpClass(cls):
+        """ Set up for class """
+        print('\n Fibonacci test for not none start: ')
+        print(' +++++++++++++ ')
+        
+    @classmethod
+    def tearDownClass(cls):
+        """ Tear down for class """
+        print(' ------------- ')
+        print(' Fibonacci test end ')
+        
+    def setUp(self):
+        """ For comfort """
+        print(' --> ')
+
+    def test_argument_unpacked_dict(self):
+        """ Current arg is dict. TypeError  """
+        self.assertIsNotNone(fib.fibon(**{'num_of_members':5}))
+
+    def test_argument_unpacked_list(self):
+        """ Current arg is list. TypeError  """
+        self.assertIsNotNone(fib.fibon(*[5]))
 
     def test_not_none(self):
         """ Is return not None """
         self.assertIsNotNone(fib.fibon(10))
+
+
+class FibonIsEqualTest(unittest.TestCase):
+    """ Fibonacci for equality test """
+    @classmethod
+    def setUpClass(cls):
+        """ Set up for class """
+        print('\n Fibonacci test for equality start: ')
+        print(' +++++++++++++ ')
         
+    @classmethod
+    def tearDownClass(cls):
+        """ Tear down for class """
+        print(' ------------- ')
+        print(' Fibonacci test end ')
+        
+    def setUp(self):
+        """ For comfort """
+        print(' --> ')
+
+    def test_return_amount(self):
+        """ Correct return length """
+        self.assertEqual(len(fib.fibon(10)), 10)
+
     def test_fibon_equal(self):
         """ Is return value is equal with example """
         self.assertEqual(fib.fibon(10),[1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
